89557ec959f8d4957b9a0b5149e809b921718b22
Harshit Pandey <harshpandeyalfa2@gmail.com>
2023-04-07 17:11:46 +0530

    link corrections 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
         <a href="mailto: harshpandeyalfa2@gmail.com" target="black">
           <BsGoogle/>
         </a>
-        <a href="https://drive.google.com/file/d/1rfxpdkPS1pFrCDx0dUXBsWB9pAxwv-cd/view?usp=sharing" target="black">
+        <a href="https://drive.google.com/file/d/1zCpReVfvOjAPXfjJ8IXmxK2tEKOUZpXi/view" target="black">
           <BsCloudUploadFill/>
         </a>
       </div>
